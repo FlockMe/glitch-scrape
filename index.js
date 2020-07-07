2a98b7d688254480b4aeea6a77301a4832806b48
hansputera <47862061+hansputera@users.noreply.github.com>
2020-07-07 19:42:55 +0700

    I do not know, why we added the platform includes

--- index.js
+++ index.js
@@ -3,19 +3,29 @@ const { author, version } = require('./package.json');
 
 const genURL = query => `https://glitch-api.herokuapp.com/api/checker?q=${query}`;
 
+let platform = ['linux', 'windows', 'ubuntu', 'macos', 'debian'];
+
 /**
 
- - Updated on Saturday, 13 June 2020
+ - Updated 7 July 2020
  - By Hanif Dwy Putra S.
  - Made with JavaScript
+
+ * Change Log
+ - Supported Platform added and some fixed bugs here.
 
 **/
 
 module.exports = class GlitchScrape {
   constructor() {
+   if (!platform.includes(process.platform)) throw Error(`We do not support the ${process.platform} platform!!`);
+
    this.version = version;
    this.author = author;
+   this.changelog = 'Fixed bugs.\nWe find the bug here on incident property of glitch scrape client package.\nNow you can use this!';
+   this.lang = ['en', 'id'];
  }
+
   async status() {
     const { body: status } = await request.get(genURL('status'));
     return status;
@@ -23,7 +33,7 @@ module.exports = class GlitchScrape {
 
  async incident() {
     const { body: incident } = await request.get(genURL('incidents'));
-    let incidentStat = incident.pastIncidentDesc;
+    let incidentStat = incident.pastIncidentsDesc;
 
     if (incidentStat.investigate === [] && incidentStat.monitoring === [] && incidentStat.updated === [] && incidentStat.resolved === []) incidentStat = `Nothing reports about something incidents happen.`;
     if (incidentStat.investigate !== [] && incidentStat.monitoring !== [] && incidentStat.updated !== [] && incidentStat.resolved !== []) incidentStat = incidentStat.resolved[0];
